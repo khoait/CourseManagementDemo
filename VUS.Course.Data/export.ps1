52813ffc470af67ffa4914c1ff884ef9f7d77f82
Khoa Nguyen <nhnkhoa@live.com>
2022-08-03 19:25:50 +0100

    added solution package, deployment package, data migration

---
+++ VUS.Course.Data/export.ps1
@@ -0,0 +1,114 @@
+﻿[CmdletBinding()]
+
+param
+(
+	[string]$CrmConnectionString, #The connection string as per CRM Sdk
+	[string]$Key #The key for the stored connection string
+)
+
+$ErrorActionPreference = "Stop"
+
+#Script Location
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+Write-Verbose "Script Path: $scriptPath"
+
+#Print Parameters
+Write-Verbose "ConnectionString = $CrmConnectionString"
+Write-Verbose "Key = $Key"
+
+#Variables
+$dataFileName = "data.zip"
+$DataFile = "$env:TEMP\configuration_data_$(get-date -f yyyyMMdd-HHmmss).zip"
+$XmlDocumentLocation = "$scriptPath\..\VUS.Course.DeploymentPackage\PkgFolder\ImportConfig.xml"
+[xml]$XmlDocument = Get-Content -Path $XmlDocumentLocation
+$XmlDocument.configdatastorage.SetAttribute("crmmigdataimportfile", "")
+$XmlDocument.Save($XmlDocumentLocation)
+
+Write-Verbose "ConnectionString = $connectionString"
+
+
+Remove-Item "$scriptPath\ConfigurationData" -Recurse -ErrorAction Ignore
+
+$matches = Get-ChildItem -Path "$scriptPath\..\VUS.Course.Tools\Tools\XrmCIFramework*" | Sort-Object Name -Descending
+if ($matches.Length -gt 0)
+{
+	$frameworkPath = $matches[0].FullName
+	Write-Verbose "Using XrmCIFramework: $frameworkPath"
+}
+else
+{
+	throw "XrmCIFramework not found in tools folder"
+}
+
+$matches = Get-ChildItem -Path "$scriptPath\..\VUS.Course.Tools\Tools\Microsoft.Xrm.Tooling.ConfigurationMigration\*" | Sort-Object Name -Descending
+if ($matches.Length -gt 0)
+{
+	$configurationMigrationPath = $matches[0].FullName
+	Write-Verbose "Using ConfigurationMigration: $configurationMigrationPath"
+}
+else
+{
+	throw "ConfigurationMigration not found in tools folder"
+}
+
+$matches = Get-ChildItem -Path "$scriptPath\..\VUS.Course.Tools\Tools\Microsoft.Xrm.Tooling.CrmConnector.PowerShell\*" | Sort-Object Name -Descending
+if ($matches.Length -gt 0)
+{
+	$crmConnectorPath = $matches[0].FullName
+	Write-Verbose "Using CrmConnector: $crmConnectorPath"
+}
+else
+{
+	throw "CrmConnector not found in tools folder"
+} 
+
+Import-Module "$frameworkPath\Xrm.Framework.CI.PowerShell.Cmdlets.psd1"
+
+if ($CrmConnectionString)
+{
+	Write-Verbose "Using supplied connection string"
+}
+else
+{
+	Write-Verbose "Using connection store"
+	$CrmConnectionString = GetXrmConnectionFromConfig($key);
+}
+
+if (Test-Path -Path "$scriptPath\Schema\schema.xml")
+{
+	$exportParams = @{
+		crmConnectionString = "$CrmConnectionString"
+		crmConnectionTimeout = 360
+		dataFile = $DataFile
+		schemaFile = "$scriptPath\Schema\schema.xml"
+		logsDirectory = "$env:TEMP"
+		configurationMigrationModulePath = "$configurationMigrationPath"
+		toolingConnectorModulePath = "$crmConnectorPath"
+	}
+
+	& "$frameworkPath\ExportCMData.ps1" @exportParams
+
+	$extractParams = @{
+		dataFile = $DataFile
+		extractFolder = "$scriptPath\Reference"
+		sortExtractedData = $true
+		splitExtractedData = $true
+	}
+
+	& "$frameworkPath\ExtractCMData.ps1" @extractParams
+
+	$packCMDataParams = @{
+		dataFile = "$scriptPath\$dataFileName"
+		extractFolder = "$scriptPath\Reference"
+		combineDataXmlFile = $true
+	}
+
+	& "$frameworkPath\PackCMData.ps1" @packCMDataParams
+
+	$XmlDocument.configdatastorage.SetAttribute("crmmigdataimportfile", $dataFileName)
+	$XmlDocument.Save($XmlDocumentLocation)
+}
+else
+{
+	Write-Verbose "No reference data schema given at $scriptPath\schema.xml"
+}
---
+++ VUS.Course.Tools/Scripts/installTools.ps1
@@ -0,0 +1,67 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = "tls12"
+
+$sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+$targetNugetExe = ".\nuget.exe"
+Remove-Item ..\Tools -Force -Recurse -ErrorAction Ignore
+Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
+Set-Alias nuget $targetNugetExe -Scope Global -Verbose
+
+##
+##Download Plugin Registration Tool
+##
+##./nuget install Microsoft.CrmSdk.XrmTooling.PluginRegistrationTool -O ..\Tools
+##md ..\Tools\PluginRegistration
+##$prtFolder = Get-ChildItem ../Tools | Where-Object {$_.Name -match 'Microsoft.CrmSdk.XrmTooling.PluginRegistrationTool.'}
+##move ..\Tools\$prtFolder\tools\*.* ..\Tools\PluginRegistration
+##Remove-Item ..\Tools\$prtFolder -Force -Recurse
+
+##
+##Download CoreTools
+##
+##./nuget install  Microsoft.CrmSdk.CoreTools -O ..\Tools
+##md ..\Tools\CoreTools
+##$coreToolsFolder = Get-ChildItem ../Tools | Where-Object {$_.Name -match 'Microsoft.CrmSdk.CoreTools.'}
+##move ..\Tools\$coreToolsFolder\content\bin\coretools\*.* ..\Tools\CoreTools
+##Remove-Item ..\Tools\$coreToolsFolder -Force -Recurse
+
+##
+##Download Configuration Migration
+##
+##./nuget install  Microsoft.CrmSdk.XrmTooling.ConfigurationMigration.Wpf -O ..\Tools
+##md ..\Tools\ConfigurationMigration
+##$configMigFolder = Get-ChildItem ../Tools | Where-Object {$_.Name -match 'Microsoft.CrmSdk.XrmTooling.ConfigurationMigration.Wpf.'}
+##move ..\Tools\$configMigFolder\tools\*.* ..\Tools\ConfigurationMigration
+##Remove-Item ..\Tools\$configMigFolder -Force -Recurse
+
+##
+##Download Package Deployer 
+##
+##./nuget install  Microsoft.CrmSdk.XrmTooling.PackageDeployment.WPF -O ..\Tools
+##md ..\Tools\PackageDeployment
+##$pdFolder = Get-ChildItem ../Tools | Where-Object {$_.Name -match 'Microsoft.CrmSdk.XrmTooling.PackageDeployment.Wpf.'}
+##move ..\Tools\$pdFolder\tools\*.* ..\Tools\PackageDeployment
+##Remove-Item ..\Tools\$pdFolder -Force -Recurse
+
+
+##
+##Download XrmCIFramework PowerShell module
+##
+./nuget install XrmCIFramework -O ..\Tools
+md -Force ..\Tools\XrmCIFramework
+$xrmCIFolder = Get-ChildItem ../Tools | Where-Object {$_.Name -match 'XrmCIFramework.'}
+move ..\Tools\$xrmCIFolder\tools\*.* ..\Tools\XrmCIFramework
+Remove-Item ..\Tools\$xrmCIFolder -Force -Recurse
+
+##
+##Remove NuGet.exe
+##
+Remove-Item nuget.exe
+
+## PSGallery should be available
+$PSVersionTable.PSVersion
+Install-PackageProvider -Name NuGet -Force -Scope CurrentUser
+
+Save-Package "microsoft.xrm.tooling.crmconnector.powershell" -Path ..\Tools
+Save-Package "microsoft.xrm.tooling.packagedeployment.powershell" -Path ..\Tools
+Save-Package "microsoft.xrm.tooling.configurationmigration" -RequiredVersion 1.0.0.61 -Path ..\Tools
+Save-Package "microsoft.powerApps.checker.powerShell" -Path ..\Tools
